f25a3c85ded3cd76ec0ea17e119bf4d8162e44b0
Stargeras <dillongrosso@live.com>
2021-08-14 20:20:50 -0400

    fixes

--- debian/01_build.sh
+++ debian/01_build.sh
@@ -4,11 +4,11 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 release=$1
 flavor=$2
 workspacedir="/srv/workspace/debianbuild"
-filesdir="${basedir}/${relase}/${flavor}"
+filesdir="${basedir}/${release}/${flavor}"
 scriptname="${flavor}.sh"
 
 # Install archiso
-sudo apt install -y debootstrap arch-build-scripts --noconfirm
+sudo apt install -y debootstrap arch-build-scripts
 
 # COPY BUILD-FILES
 #sudo chown -R jenkins:users ${filesdir}
@@ -28,7 +28,7 @@ sudo cp ${workspacedir}/build-files/* ${workspacedir}/chroot/root/
 
 # RUN SCRIPT
 #sudo arch-chroot ${workspacedir}/work/x86_64/airootfs chmod 777 /root/${scriptname}
-sudo arch-chroot ${workspacedir}chroot /bin/bash -c "su - -c /root/${scriptname}"
+sudo arch-chroot ${workspacedir}/chroot /bin/bash -c "su - -c /root/${scriptname}"
 
 # RUN FINAL STAGE
 sudo lb binary
