35a79c458fc36e393c15d8152bc59b47473ef824
Stargeras <dillongrosso@live.com>
2021-08-23 14:35:32 -0400

    update http

--- archlinux/02_http_upload.sh
+++ archlinux/02_http_upload.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 workspacedir="/srv/workspace/archbuild"
+package="apache"
 
 # SERVE OVER HTTP
-if ! pacman -Q apache > /dev/null 2>&1; then
-  sudo pacman -S apache --noconfirm
+if ! pacman -Q ${package} > /dev/null 2>&1; then
+  sudo pacman -S ${package} --noconfirm
 fi
 sudo mkdir -p /srv/http/releases
 sudo systemctl restart httpd
