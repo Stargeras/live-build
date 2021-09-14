006075d37779e274705b9b5a64a70f2f489e711b
Stargeras <dillongrosso@live.com>
2021-09-13 22:07:20 -0400

    combine http

--- archlinux/01_build.sh
+++ archlinux/01_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export serveoverhttp=true
-export removeworkspaceafterbuild=true
-export basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-export flavor=$1
-export workspacedir="${basedir}/workspace"
-export filesdir="${basedir}/${flavor}"
-export scriptname="${flavor}.sh"
+serveoverhttp=true
+removeworkspaceafterbuild=true
+basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+flavor=$1
+workspacedir="${basedir}/workspace"
+filesdir="${basedir}/${flavor}"
+scriptname="${flavor}.sh"
 
 # Install archiso
 package="archiso"
@@ -47,9 +47,25 @@ sudo mkarchiso -v .
 now=$(date +"%m%d%Y")
 sudo mv ${workspacedir}/out/* ${workspacedir}/archlinux-${flavor}-${now}.iso
 
-# RUN 02 HTTP
+# SERVE OVER HTTP
 if ${serveoverhttp}; then
-  bash ${basedir}/02_http_upload.sh
+  package="apache"
+  if ! pacman -Q ${package} > /dev/null 2>&1; then
+    sudo pacman -S ${package} --noconfirm
+  fi
+  sudo mkdir -p /srv/http/releases
+  sudo systemctl restart httpd
+  sudo mv ${workspacedir}/*.iso /srv/http/releases/
+  ips=$(ip a | grep "scope" | grep -Po '(?<=inet )[\d.]+')
+  echo "build available at:"
+  for ip in ${ips}; do
+    echo "http://${ip}/releases/archlinux-${flavor}-${now}.iso"
+  done
 else
   echo "build available at ${workspacedir}/archlinux-${flavor}-${now}.iso"
+fi
+
+# REMOVE WORKSPACE
+if ${removeworkspaceafterbuild}; then
+  sudo rm -rf ${workspacedir}
 fi
