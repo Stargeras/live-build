5d8f103529e79db229cddfe411432ea2bc54fc1a
Stargeras <dillongrosso@live.com>
2021-09-18 21:46:18 -0400

    uservar updates

--- archlinux/gnome/gnome.sh
+++ archlinux/gnome/gnome.sh
@@ -4,6 +4,8 @@ packages="gnome firefox chromium neofetch htop gparted gnome-tweaks code ttf-dro
         celluloid gedit htop freerdp"
 #packages="gnome-session gnome-tweaks gnome-terminal gnome-backgrounds gnome-calculator gnome-control-center nautilus bluez gdm pulseaudio firefox chromium neofetch htop gparted virt-viewer cups networkmanager-openvpn networkmanager transmission-gtk rhythmbox celluloid gedit htop eog freerdp"
 aurpackages="f5vpn cackey gnome-shell-extension-dash-to-panel"
+builddir="/srv/build-files"
+username=$(cat ${builddir}/username)
 pacman -S ${packages} --noconfirm #code npm nodejs deepin-community-wallpapers
 systemctl enable NetworkManager
 systemctl enable gdm
@@ -13,38 +15,38 @@ systemctl enable cups-browsed
 # AUR Pakages
 for package in ${aurpackages}; do
   aurdir="~/Documents/build"
-  su admin -c "mkdir -p ${aurdir}"
-  su admin -c "cd ${aurdir} && git clone https://aur.archlinux.org/${package}.git"
-  su admin -c "cd ${aurdir}/${package}/ && makepkg -si --noconfirm"
-  su admin -c "sudo rm -rf ${aurdir}"
+  su ${username} -c "mkdir -p ${aurdir}"
+  su ${username} -c "cd ${aurdir} && git clone https://aur.archlinux.org/${package}.git"
+  su ${username} -c "cd ${aurdir}/${package}/ && makepkg -si --noconfirm"
+  su ${username} -c "sudo rm -rf ${aurdir}"
 done
 
 # #yaru
 #pacman -S ninja meson sassc --noconfirm
-#su admin -c 'mkdir ~/Documents/build'
-#su admin -c 'cd ~/Documents/build && git clone https://github.com/ubuntu/yaru.git'
-#su admin -c 'cd ~/Documents/build/yaru/ && sudo ./bootstrap.sh -b'
+#su ${username} -c 'mkdir ~/Documents/build'
+#su ${username} -c 'cd ~/Documents/build && git clone https://github.com/ubuntu/yaru.git'
+#su ${username} -c 'cd ~/Documents/build/yaru/ && sudo ./bootstrap.sh -b'
 
-#su admin -c 'cd ~/Documents/build/ && git clone https://github.com/jaxwilko/gtk-theme-framework.git'
-#su admin -c 'cd ~/Documents/build/gtk-theme-framework/ && sudo ./main.sh -io -t amarena -d /usr/share/themes/ -p /usr/share/icons/'
-#su admin -c 'cd ~/Documents/build/gtk-theme-framework/ && sudo ./main.sh -io -t palenight -d /usr/share/themes/ -p /usr/share/icons/'
-#su admin -c 'cd ~/Documents/build/gtk-theme-framework/ && sudo ./main.sh -io -t gruvterial -d /usr/share/themes/ -p /usr/share/icons/'
+#su ${username} -c 'cd ~/Documents/build/ && git clone https://github.com/jaxwilko/gtk-theme-framework.git'
+#su ${username} -c 'cd ~/Documents/build/gtk-theme-framework/ && sudo ./main.sh -io -t amarena -d /usr/share/themes/ -p /usr/share/icons/'
+#su ${username} -c 'cd ~/Documents/build/gtk-theme-framework/ && sudo ./main.sh -io -t palenight -d /usr/share/themes/ -p /usr/share/icons/'
+#su ${username} -c 'cd ~/Documents/build/gtk-theme-framework/ && sudo ./main.sh -io -t gruvterial -d /usr/share/themes/ -p /usr/share/icons/'
 
-# su admin -c 'cat >> ~/.bashrc << EOF
+# su ${username} -c 'cat >> ~/.bashrc << EOF
 # export PS1="\[\e[31m\]\u\[\e[m\]@\h\[\e[34m\]\w\[\e[m\]\\$ "
 # EOF'
 
-mkdir -p /home/admin/.config/autostart
-cat > /home/admin/.config/autostart/script.desktop << EOF
+mkdir -p /home/${username}/.config/autostart
+cat > /home/${username}/.config/autostart/script.desktop << EOF
 [Desktop Entry]
 Name=script
 GenericName=config script
-Exec=/home/admin/Documents/config.sh
+Exec=${builddir}/config.sh
 Terminal=false
 Type=Application
 X-GNOME-Autostart-enabled=true
 EOF
-chmod +x /home/admin/.config/autostart/script.desktop
+chmod +x /home/${username}/.config/autostart/script.desktop
 
 #Firefox
 cat >> /usr/lib/firefox/browser/defaults/preferences/vendor.js << EOF
@@ -54,12 +56,13 @@ pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-
 EOF
 
 ##Set XORG as default session
-cat > /var/lib/AccountsService/users/admin << EOF
+cat > /var/lib/AccountsService/users/${username} << EOF
 [User]
 Language=
 XSession=gnome-xorg
 EOF
 
-cp /root/* /home/admin/Documents/
-chmod +x /home/admin/Documents/*.sh
-chown -R admin:users /home/admin/
+# Permissions
+chmod +x ${builddir}/*.sh
+chown -R ${username}:users ${builddir}
+chown -R ${username}:users /home/${username}/
