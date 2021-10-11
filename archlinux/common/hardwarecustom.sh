b76aff86e299f7e4c602050e66bc25c5ccdfc16d
Stargeras <64556423+Stargeras@users.noreply.github.com>
2021-10-11 09:37:10 -0400

    Update hardwarecustom.sh

--- archlinux/common/hardwarecustom.sh
+++ archlinux/common/hardwarecustom.sh
@@ -1,1 +1,4 @@
-xrandr --output HDMI-1 --primary --pos 0x0 --output HDMI-2 --rotate left --right-of HDMI-1 --output DP-2 --right-of HDMI-2
+xrandr \
+    --output HDMI-1 --primary --pos 0x0 \
+    --output HDMI-2 --rotate left --right-of HDMI-1 \
+    --output DP-2 --right-of HDMI-2
