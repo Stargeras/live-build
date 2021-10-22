07706b1cf4c2c0239503e102099745e1b0b69838
Stargeras <dillongrosso@live.com>
2021-10-22 16:21:56 -0400

    add gnome dock

--- archlinux/gnome/gnome.sh
+++ archlinux/gnome/gnome.sh
@@ -3,7 +3,7 @@ packages="gnome firefox chromium neofetch htop gparted gnome-tweaks code ttf-dro
         virt-viewer cups networkmanager networkmanager-openvpn transmission-gtk rhythmbox \
         celluloid gedit htop freerdp"
 #packages="gnome-session gnome-tweaks gnome-terminal gnome-backgrounds gnome-calculator gnome-control-center nautilus bluez gdm pulseaudio firefox chromium neofetch htop gparted virt-viewer cups networkmanager-openvpn networkmanager transmission-gtk rhythmbox celluloid gedit htop eog freerdp"
-aurpackages="f5vpn cackey gnome-shell-extension-dash-to-panel"
+aurpackages="f5vpn cackey gnome-shell-extension-dash-to-panel gnome-shell-extension-dash-to-dock"
 builddir="/srv/build-files"
 username=$(cat ${builddir}/username)
 pacman -S ${packages} --noconfirm #code npm nodejs deepin-community-wallpapers
