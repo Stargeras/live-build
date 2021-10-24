4b8d719dda0943536889d3a784f264e2431f55a0
Stargeras <64556423+Stargeras@users.noreply.github.com>
2021-10-24 17:22:49 -0400

    Create config.sh

---
+++ archlinux/cinnamon/config.sh
@@ -0,0 +1,16 @@
+gsettings set org.cinnamon.desktop.background picture-uri 'file:///usr/share/backgrounds/gnome/Road.jpg'
+gsettings set org.cinnamon favorite-apps "['firefox.desktop', 'nemo.desktop', 'org.gnome.Terminal.desktop', 'cinnamon-settings.desktop']"
+gsettings set org.nemo.window-state geometry '1175x684+376+115'
+dconf write /org/gnome/desktop/interface/monospace-font-name "'Liberation Mono 11'"
+gsettings set org.cinnamon.theme name 'Adapta-Nokto'
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Adapta-Nokto'
+gsettings set org.cinnamon.desktop.interface icon-theme 'breeze-dark'
+gsettings set org.cinnamon.desktop.interface cursor-theme 'breeze_cursors'
+gsettings set org.cinnamon.desktop.wm.preferences theme 'Adapta-Nokto'
+gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+gsettings set org.gnome.Terminal.Legacy.Settings theme-variant dark
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ default-size-rows 27
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ default-size-columns 122
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.cinnamon.desktop.interface clock-use-24h false
+gsettings set org.cinnamon.desktop.interface clock-show-date true
