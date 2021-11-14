42254a50fe0806ef591e7668269089c7728e9fb5
Stargeras <64556423+Stargeras@users.noreply.github.com>
2021-11-13 23:12:21 -0500

    Update hardwarecustom.sh

--- debian/common/hardwarecustom.sh
+++ debian/common/hardwarecustom.sh
@@ -1,4 +1,18 @@
 xrandr \
-    --output HDMI-1 --primary --pos 0x0 \
-    --output HDMI-2 --rotate left --right-of HDMI-1 \
-    --output DP-2 --right-of HDMI-2
+--output HDMI-1 \
+--crtc 2 \
+--mode 1920x1080 \
+--pos 1920x0 \
+--rate 60.00 \
+--rotate left \
+--output DP-2 \
+--crtc 1 \
+--mode 1920x1080 \
+--pos 3000x374 \
+--rate 60.00 \
+--output HDMI-2 \
+--crtc 0 \
+--mode 1920x1080 \
+--pos 0x408 \
+--primary \
+--rate 144.00
