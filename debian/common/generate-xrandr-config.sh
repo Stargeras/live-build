0e1823552c5ecb14714174d87a5a1964fa5b0088
Stargeras <dillongrosso@live.com>
2021-12-01 12:00:09 -0500

    add xrandr config generation

--- debian/common/generate-xrandr-config.sh
+++ debian/common/generate-xrandr-config.sh
@@ -3,3 +3,4 @@ package="autorandr"
 
 sudo apt install -y autorandr
 echo "xrandr $(autorandr --config | sed "s/^/--/g" | sed 's/$/ \\/g' | sed '/crtc/d')" > new-xrandr.sh
+echo "new xrandr config at $(pwd)/new-xrandr.sh"
