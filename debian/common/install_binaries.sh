3e1079d8ce4a16300657eff2f290f114d27a08d7
Stargeras <64556423+Stargeras@users.noreply.github.com>
2022-03-23 16:17:28 -0400

    dynamic terraform version

--- debian/common/install_binaries.sh
+++ debian/common/install_binaries.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # THIS DOESN'T RUN AT INSTALL TIME. MUST RUN WHILE IN OS ENVIRONMENT
+terraformversion=$(curl https://releases.hashicorp.com/terraform/ | grep href | sort -r | grep terraform_ | head -1 | awk -F _ '{print $NF}' | awk -F '<' '{print $1}')
 
 urls="https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator \
 https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl \
-https://releases.hashicorp.com/terraform/1.1.0/terraform_1.1.0_linux_amd64.zip"
+https://releases.hashicorp.com/terraform/${terraformversion}/terraform_${terraformversion}_linux_amd64.zip"
 
 for url in ${urls}; do
   file=$(echo ${url} | awk -F / '{print $NF}')
