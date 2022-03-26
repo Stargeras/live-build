03324cdb010bf71ccfefa7e75581a5ecba3e30a9
Stargeras <dillongrosso@live.com>
2022-03-26 19:30:06 -0400

    material shell

--- debian/bullseye/gnome/gnome.sh
+++ debian/bullseye/gnome/gnome.sh
@@ -71,8 +71,16 @@ cp -r zorin-icon-themes/Zorin* /usr/share/icons
 rm -rf zorin-desktop-themes
 rm -rf zorin-icon-themes
 
-#Material-shell
-git clone https://github.com/material-shell/material-shell.git /usr/share/gnome-shell/extensions/material-shell@papyelgringo
+# Material-shell
+#git clone https://github.com/material-shell/material-shell.git /usr/share/gnome-shell/extensions/material-shell@papyelgringo
+url="https://github.com/material-shell/material-shell/archive/refs/tags/12.zip"
+file=$(echo ${url} | awk -F / '{print$NF}')
+dir="/usr/share/gnome-shell/extensions/material-shell@papyelgringo"
+mkdir ${dir}
+wget ${url}
+unzip ${file} -d $(echo ${file} | awk -F . '{print $1}')
+mv $(echo ${file} | awk -F . '{print $1}')/$(ls $(echo ${file} | awk -F . '{print $1}'))/* ${dir}/
+rm -rf $(echo ${file} | awk -F . '{print $1}')*
 
 #disable wayland gdm
 #echo 'WaylandEnable=false' >> /etc/gdm3/custom.conf
