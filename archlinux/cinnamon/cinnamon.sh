689dc5790b43428e353ae6e13040ce5a49fb107d
Stargeras <dillongrosso@live.com>
2022-04-28 15:14:03 -0400

    cinnamon updates

--- archlinux/cinnamon/cinnamon.sh
+++ archlinux/cinnamon/cinnamon.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 packages="cinnamon nemo-fileroller lightdm lightdm-gtk-greeter firefox chromium neofetch htop gparted celluloid gnome-terminal gedit \
-          virt-viewer freerdp imwheel cups vim curl gnome-screenshot eog code archlinux-wallpaper jq \
+          virt-viewer freerdp imwheel cups vim curl gnome-screenshot eog code archlinux-wallpaper jq terraform kubectl helm \
           noto-fonts inter-font ttf-opensans ttf-roboto ttf-roboto-mono ttf-droid"
 aurpackages="f5vpn cackey yay orchis-theme-git tela-circle-icon-theme-git breeze-hacked-cursor-theme-git"
 builddir="/srv/build-files"
 username=$(cat ${builddir}/username)
-usechromiummods="true"
+usechromiummods="false"
 
 pacman -S ${packages} --noconfirm
 systemctl enable NetworkManager
