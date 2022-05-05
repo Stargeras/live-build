aa1a5b72eca851f088c999b017e4e2cf9d4d93a7
Stargeras <dillongrosso@live.com>
2022-05-05 11:07:24 -0400

    xorg to wayland

--- archlinux/gnome-ubuntu/gnome-ubuntu.sh
+++ archlinux/gnome-ubuntu/gnome-ubuntu.sh
@@ -45,13 +45,6 @@ pref("browser.tabs.drawInTitlebar", true);
 pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"library-button\",\"sidebar-button\",\"fxa-toolbar-menu-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\"],\"dirtyAreaCache\":[],\"currentVersion\":16,\"newElementCount\":2}");
 EOF
 
-##Set XORG as default session
-cat > /var/lib/AccountsService/users/${username} << EOF
-[User]
-Language=
-XSession=gnome-xorg
-EOF
-
 # Download ubuntu wallpaper
 wget ${wallpaperurl} -O /usr/share/backgrounds/ubuntu.jpg
 
