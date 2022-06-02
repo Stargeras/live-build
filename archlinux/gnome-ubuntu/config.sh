311b74432347b3eb1939ba9ca5a5a06245c819e0
Stargeras <dillongrosso@live.com>
2022-06-01 20:12:39 -0400

    fix dock extension

--- archlinux/gnome-ubuntu/config.sh
+++ archlinux/gnome-ubuntu/config.sh
@@ -32,7 +32,7 @@ dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6
 
 # Extensions
 gsettings set org.gnome.shell favorite-apps "['chromium.desktop', 'nautilus.desktop', 'gnome-terminal.desktop']"
-gsettings set org.gnome.shell enabled-extensions "['ubuntu-dock@ubuntu.com']"
+gsettings set org.gnome.shell enabled-extensions "['dash-to-dock@micxgx.gmail.com']"
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink 'true'
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed 'true'
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'LEFT'
